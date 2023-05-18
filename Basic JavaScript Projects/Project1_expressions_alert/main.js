cd7a4dbaa9eb2372f8a58c4f0ecb98b35afee4ea
mslysz <macslysz@gmail.com>
2023-05-18 08:41:01 +0100

    added html and javascript file

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,14 @@
+// I have created an alert in the window and in the document
+window.alert("Hello !!!");
+document.write("Test text");
+
+// I created two variables and assigned them a string
+let Sent1 = " This is the beginning of the string"
+let Sent2 = " and this is the end of the string"
+
+// I used concatenation to add two variables together 
+// and display them in the document.
+document.write(Sent1 + Sent2);
+
+// Write an expression
+document.write(20 / 4);
