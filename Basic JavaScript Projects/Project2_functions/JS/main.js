6eb622c0115ac3c0970dd1b20432170dac54da89
mslysz <macslysz@gmail.com>
2023-05-20 07:46:13 +0100

    Added 2nd project

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+// I have created a function to change paragraph styles
+function colorChange() {
+  document.getElementById('paragraph').style.color = "blue"
+
+}
+
+
+// I created a function, assigned a string to a variable, used 
+// concatenation to add an additional string to the variable
+function myFunction() {
+  let history = "I can learn ";
+  history += "a lot from this book";
+  document.getElementById('h1p').innerHTML = history;
+}
